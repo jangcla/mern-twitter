225b32cd2e35192a64747c68e59f3359edadbd11
jangcla <jangcla@ymail.com>
2021-08-16 10:25:02 -0700

    it wouldnt push

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./config/keys").mongoURI
+const users = require("./routes/api/tweets")
+const tweets = require("./routes/api/tweets")
 
-
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
 
 app.get("/", (rew, res) => {
     res.send("Hello John!")
 })
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 const port = process.env.PORT || 5000;
 
